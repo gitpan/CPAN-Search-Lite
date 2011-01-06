ddf8b01bf6fbfe2c4cc5852f4f4a91d150c5b349
Kenichi Ishigaki <ishigaki@cpan.org>
2011-01-06 07:23:46 +0000

    Import of ISHIGAKI/CPAN-Search-Lite-0.77 from CPAN.
    gitpan-cpan-distribution: CPAN-Search-Lite
    gitpan-cpan-version:      0.77
    gitpan-cpan-path:         ISHIGAKI/CPAN-Search-Lite-0.77.tar.gz
    gitpan-cpan-author:       ISHIGAKI
    gitpan-cpan-maturity:     released

--- lib/CPAN/Search/Lite/Lang.pm
+++ lib/CPAN/Search/Lite/Lang.pm
@@ -1,7 +1,7 @@
 package CPAN::Search::Lite::Lang;
 use strict;
 use warnings;
-our $VERSION = 0.76;
+our $VERSION = 0.77;
 
 use utf8;
 use base qw(Exporter);
@@ -60,7 +60,7 @@ used as
    load(lang => $lang, pages => $pages, chaps_desc => $chaps_desc);
 
 which will, for example, take the data structure C<$pages> and
-populate C<$pages->{$lang}> with the appropriate C<$page> from
+populate C<< $pages->{$lang} >> with the appropriate C<$page> from
 the requested C<$pages> from C<CPAN::Search::Lite::Lang::$lang.pm>.
 At present the available data structures are:
 
